c8a5d388bede2b1af2a46bec094f378e79ff8c99
Moises Goncalves <moises_goncalves@outlook.com.br>
2024-05-12 22:17:23 -0300

    Conclusao do Controller de Lancamentos

--- src/main/java/com/springweb/minhasfinancas/service/impl/LancamentoServiceImpl.java
+++ src/main/java/com/springweb/minhasfinancas/service/impl/LancamentoServiceImpl.java
@@ -3,6 +3,7 @@ package com.springweb.minhasfinancas.service.impl;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
@@ -13,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.springweb.minhasfinancas.exception.RegraNegocioException;
 import com.springweb.minhasfinancas.model.entity.Lancamento;
 import com.springweb.minhasfinancas.model.enums.StatusLancamento;
+import com.springweb.minhasfinancas.model.enums.TipoLancamento;
 import com.springweb.minhasfinancas.model.repository.LancamentoRepository;
 import com.springweb.minhasfinancas.service.LancamentoService;
 
@@ -89,6 +91,29 @@ public class LancamentoServiceImpl implements LancamentoService {
 		if(lancamento.getTipo() == null) {
 			throw new RegraNegocioException("Informe um Tipo de Lançamento!");
 		}
+	}
+
+	@Override
+	public Optional<Lancamento> obterPorId(Long id) {
+		return repository.findById(id);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public BigDecimal obterSaldoPorUsuario(Long id) {
+		
+		BigDecimal receitas = repository.obterSaldoPorTipoLancamentoEusuario(id, TipoLancamento.RECEITA);
+		BigDecimal despesas = repository.obterSaldoPorTipoLancamentoEusuario(id, TipoLancamento.DESPESA);
+		
+		if(receitas == null) {
+			receitas = BigDecimal.ZERO;
+		}
+		
+		if(despesas == null) {
+			despesas = BigDecimal.ZERO;
+		}
+		
+		return receitas.subtract(despesas);
 	}
 
 }
--- src/main/java/com/springweb/minhasfinancas/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/springweb/minhasfinancas/service/impl/UsuarioServiceImpl.java
@@ -51,5 +51,10 @@ public class UsuarioServiceImpl implements UsuarioService {
 			throw new RegraNegocioException("Já existe um usuário cadastrado com esse email!");
 		}
 	}
+	
+	@Override
+	public Optional<Usuario> obterPorId(Long id) {
+		return repository.findById(id);
+	}
 
 }
