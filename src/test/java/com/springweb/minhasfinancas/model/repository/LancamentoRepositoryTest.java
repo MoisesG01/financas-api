fe57ea52e07f0de1647d0aba3e3b229fe48d30ac
Moises Goncalves <moises_goncalves@outlook.com.br>
2024-05-30 22:04:05 -0300

    Finalização do back-end

--- src/test/java/com/springweb/minhasfinancas/model/repository/LancamentoRepositoryTest.java
+++ src/test/java/com/springweb/minhasfinancas/model/repository/LancamentoRepositoryTest.java
@@ -7,20 +7,20 @@ import java.time.LocalDate;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.springweb.minhasfinancas.model.entity.Lancamento;
 import com.springweb.minhasfinancas.model.enums.StatusLancamento;
 import com.springweb.minhasfinancas.model.enums.TipoLancamento;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 @ActiveProfiles("test")
